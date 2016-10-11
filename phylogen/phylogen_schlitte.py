8d4ce2d498bd07503b9d771184a02e9c6206f4a8
Jamie Macpherson <Jamie@Bern.dhcp.thecrick.org>
2016-10-11 18:04:03 +0100

    biparts selection cracked

--- phylogen/phylogen_schlitte.py
+++ phylogen/phylogen_schlitte.py
@@ -16,6 +16,35 @@
 # Imports
 #____________________________________________________________________________
 import dendropy
+import argparse
+import os.path
+import sys
+
+#____________________________________________________________________________
+# Parse commandline arguments
+#____________________________________________________________________________
+
+# check if input file exists
+def is_valid_file(arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist! Use the --help flag for input options." % arg)
+    else:
+	return arg
+
+parser = argparse.ArgumentParser(description='Model phylogenetic tree bipartitions')
+
+parser.add_argument("-t", dest="treefile", required=True,
+                    help="Phylogenetic tree file (Newick format)",
+                    type=lambda x: is_valid_file(x))
+
+
+parser.add_argument("-a", dest="alignmentfile", required=True,
+                    help="Alignment file (Fasta format)",
+                    type=lambda x: is_valid_file(x))
+
+
+args = parser.parse_args()
+
 #____________________________________________________________________________
 # Read phylogenetic data
 #____________________________________________________________________________
@@ -31,10 +60,15 @@ def itree(tree, alignment):
 itree(t,a)
 
 def getbipart(tree):
-	getbiparts.biparts=[]
+	getbipart.biparts=[]
 	for node in itree.tree:
-		getbiparts.biparts.append(node.edge.bipartition.leafset_taxa(itree.taxa)) 
+		if node.edge.bipartition.leafset_as_newick_string(itree.taxa).find("KPYR") != -1:
+			print node.edge.bipartition.leafset_as_newick_string(itree.taxa)
+		
+		getbipart.biparts.append(node.edge.bipartition.leafset_as_newick_string(itree.taxa)) 
+	np.savetxt('bipartitions.dat', getbipart.biparts, delimiter='', fmt="%s")
+	getbipart.matching = [s for s in getbipart.biparts if 'KPYR' in s]
 	
+getbipart(t)
 
-	matching = [s for s in biparts if 'KPYR_HUMAN/85-438' in s]
 
