805a3dc01b37011829c3c98836e5e3dbe3edefaf
Jamie Macpherson <Jamie@Bern-2.local>
2016-11-07 11:01:58 +0000

    added papers, modified python script

--- phylogen/format_newick.sh
+++ phylogen/format_newick.sh
@@ -4,4 +4,10 @@ sed -n -e '/KPYR HUMAN/{p;n;}' bipartion.dat > human_bipart.dat
 awk -F\' '{ for (i=3; i<=NF; i+=2) $i=""} 1' human_bipart.dat > human_newicks1.dat
 cut -d " " -f 4- human_newicks1.dat > human_newicks.dat
 awk '{printf "%d\t%s\n", NR, $0}' human_newicks.dat >  human_newicks.dat
+
+mkdir bipartitions
+while read -r filename content ; do
+	    printf '%s\n' "$content" >> bipartitions/"${filename}.seq"
+    done < human_newicks.dat
+
 sed 's/>//' P1-initial.fasta > alignment_file.fasta
