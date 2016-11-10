a2b2f11443aa770f8817703114816cdef62de644
Jamie Macpherson <Jamie@Bern-2.local>
2016-11-10 14:00:38 +0000

    time dependent entropy calculation

--- entropy_python/ana_scripts/split_traj.sh
+++ entropy_python/ana_scripts/split_traj.sh
@@ -9,27 +9,26 @@ fi
 python = '/usr/bin/python'
 schlitter = '/Users/Jamie/jm.software/development/entropy/GSA_schlitte/entropy_python/src/schlitter.py'
 
+
+gmx trjconv -f $1 -s $2 -split 8000 
+
 let k=1
-let nst=2
+let nst=9
 
 while [ $k -le $nst ]
 do
 
 mkdir $k
-gmx make_ndx -f $2 -o $k/$k".ndx" <<EOF
-r $k
-q
-EOF
-
-gmx trjconv -f $1 -s $2 -n $k/$k".ndx" -o $k/$k.xtc <<EOF
-14
-EOF
+mv trajout$k.xtc $k
 
 mdconvert $k/$k.xtc -o $k/$k.dcd
-cd $k $python $schlitter $k.dcd $2 
+
+cd $k 
+$python $schlitter $k.dcd $2 
 cd ..
 rm $k/$k.xtc
 
 let k=k+1
 done
 exit
+
