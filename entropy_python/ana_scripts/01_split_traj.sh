3f4fd968ee7cc624a9a13b15b6ca682ccf874d43
Jamie Macpherson <Jamie@Bern.dhcp.thecrick.org>
2016-11-15 15:07:09 +0000

    analysis script

--- entropy_python/ana_scripts/01_split_traj.sh
+++ entropy_python/ana_scripts/01_split_traj.sh
@@ -6,8 +6,8 @@ then
 	exit 1
 fi
 
-schlitter='/home/jamie/jm.software/GSA_schlitte/entropy_python/src/schlitter.py'
-mdconvert='/home/jamie/jm.software/GSA_schlitte/entropy_python/ana_scripts/mdconvert.py'
+schlitter='/Users/Jamie/jm.software/GSA_schlitte/entropy_python/src/schlitter.py'
+#mdconvert='/Users/Jamie/jm.software/GSA_schlitte/entropy_python/ana_scripts/mdconvert.py'
 
 
 let k=$3
@@ -18,17 +18,17 @@ do
 
 
 
-trjconv -f $1 -s $2 -b 0 -e $k -o trajout$k.xtc -n $5 <<EOF
+gmx trjconv -f $1 -s $2 -b 0 -e $k -o trajout$k.xtc -n $5 <<EOF
 3
 EOF
 
-trjconv -f $1 -s $2 -b 0 -e 0 -o topol$k.pdb -n $5 <<EOF
+gmx trjconv -f $1 -s $2 -b 0 -e 0 -o topol$k.pdb -n $5 <<EOF
 3
 EOF
 
 mkdir $k
 cd $k
-python $mdconvert ../trajout$k.xtc -o $k.dcd
+python mdconvert ../trajout$k.xtc -o $k.dcd
 
  
 python $schlitter -t $k.dcd -s ../topol$k.pdb 
