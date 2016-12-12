773c3e57195deaa19451a4dc301d28f43f0484f1
jkleinj <jens@jkleinj.eu>
2016-12-12 11:50:00 +0000

    correction in R computation of Schlitter entropy

--- schlitter/src/gettraj.c
+++ schlitter/src/gettraj.c
@@ -76,10 +76,10 @@ int read_gromos_traj(Traj *traj, Arg *arg, int protEnd)
 				&(traj->frame[traj->nFrame].trajatom[*pnAtom].pos.z)) 
 				== 3) {
 
-				/* convert from nm to A */
-				//traj->frame[traj->nFrame].trajatom[*pnAtom].pos.x *= 10;
-				//traj->frame[traj->nFrame].trajatom[*pnAtom].pos.y *= 10;
-				//traj->frame[traj->nFrame].trajatom[*pnAtom].pos.z *= 10;
+				/* convert from A to nm */
+				traj->frame[traj->nFrame].trajatom[*pnAtom].pos.x /= 10;
+				traj->frame[traj->nFrame].trajatom[*pnAtom].pos.y /= 10;
+				traj->frame[traj->nFrame].trajatom[*pnAtom].pos.z /= 10;
 
 				/* count atoms in this frame */
 				++ *pnAtom;
